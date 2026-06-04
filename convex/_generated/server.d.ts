17d10bc4d85e53502051de5ed8f8276737a8e696
webcorddigital <webcord.digital@gmail.com>
2026-06-05 01:55:21 +0530

    fix: mock generated server types

---
+++ convex/_generated/server.d.ts
@@ -0,0 +1,7 @@
+export declare const query: any;
+export declare const mutation: any;
+export declare const internalQuery: any;
+export declare const internalMutation: any;
+export declare const action: any;
+export declare const internalAction: any;
+export declare const httpAction: any;
