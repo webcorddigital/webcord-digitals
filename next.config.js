af3b7dc7c50a9fce3ced2dd618c01805bbc35f5a
webcorddigital <webcord.digital@gmail.com>
2026-06-05 01:39:27 +0530

    feat: configure convex and image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["convex"],
+  images: {
+    domains: ["webcord-in.convex.cloud"],
+  },
 };
 
 module.exports = nextConfig;
