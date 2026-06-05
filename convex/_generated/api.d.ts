ce87072f1d6333b3c4a729f8b30a9175b7e56537
webcorddigital <webcord.digital@gmail.com>
2026-06-05 22:26:03 +0530

    chore: update generated api types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,2 +1,57 @@
-export declare const api: any;
-export declare const internal: any;
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as auth from "../auth.js";
+import type * as plans from "../plans.js";
+import type * as reviews from "../reviews.js";
+import type * as settings from "../settings.js";
+import type * as works from "../works.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  plans: typeof plans;
+  reviews: typeof reviews;
+  settings: typeof settings;
+  works: typeof works;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
